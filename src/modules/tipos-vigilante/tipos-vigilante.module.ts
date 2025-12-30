95cf19b49c890e9fe19996b7cfe18cd89f4ad315
Javisolarte <settingspyp@gmail.com>
2025-12-29 20:41:48 -0500

    Api de empleados mejorada  tipos de vigilante , contratos , perosnal , horarios de oficina 2

--- src/modules/tipos-vigilante/tipos-vigilante.module.ts
+++ src/modules/tipos-vigilante/tipos-vigilante.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TiposVigilanteService } from './tipos-vigilante.service';
 import { TiposVigilanteController } from './tipos-vigilante.controller';
 import { SupabaseModule } from '../supabase/supabase.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [SupabaseModule],
+    imports: [SupabaseModule, AuthModule],
     controllers: [TiposVigilanteController],
     providers: [TiposVigilanteService],
     exports: [TiposVigilanteService],
