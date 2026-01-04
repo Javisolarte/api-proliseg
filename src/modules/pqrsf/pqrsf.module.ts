ec3471bca44f534e1e4ece6ef595e5d30f7a20ef
Javisolarte <settingspyp@gmail.com>
2026-01-04 03:50:33 -0500

    modulo de pqrsf nuevo 2

--- src/modules/pqrsf/pqrsf.module.ts
+++ src/modules/pqrsf/pqrsf.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { PqrsfService } from './pqrsf.service';
 import { PqrsfController } from './pqrsf.controller';
 
+import { AuthModule } from '../auth/auth.module';
+import { SupabaseModule } from '../supabase/supabase.module';
+
 @Module({
+    imports: [AuthModule, SupabaseModule],
     controllers: [PqrsfController],
     providers: [PqrsfService],
     exports: [PqrsfService]
