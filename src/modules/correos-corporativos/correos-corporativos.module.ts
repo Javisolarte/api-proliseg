129c54595922295e860ceeeb94e9105d043c8745
Javisolarte <settingspyp@gmail.com>
2026-01-19 05:07:47 -0500

    Correos 2

--- src/modules/correos-corporativos/correos-corporativos.module.ts
+++ src/modules/correos-corporativos/correos-corporativos.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CorreosCorporativosService } from './correos-corporativos.service';
 import { CorreosCorporativosController } from './correos-corporativos.controller';
 import { SupabaseModule } from '../supabase/supabase.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [SupabaseModule],
+    imports: [SupabaseModule, AuthModule],
     controllers: [CorreosCorporativosController],
     providers: [CorreosCorporativosService],
     exports: [CorreosCorporativosService]
